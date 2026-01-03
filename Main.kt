3c777c92497d7085bab033f4c25b976ba9ada9f6
Maria Isabella <isabellaac837@gmail.com>
2026-01-03 15:53:09 -0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    val name = readln( )
+
+    if (name == "John") {
+        println("Hi, John! Let´s make your signature!")
+
+    } else if (name != "John")
+        println("Welcome. Put your data on the space below.")
+}
