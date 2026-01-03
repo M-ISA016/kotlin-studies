d60781fe60f9538dbb6e7a2b95ec5c7559ce1973
Maria Isabella <isabellaac837@gmail.com>
2026-01-03 16:26:42 -0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,37 @@
+fun main() { // SITE: kotlintutorial.io
+    val vehicle1: Vehicle = Vehicle() // Here, an object of type 'Vehicle' is created and assigned to the variable 'vehicle1'
+
+    vehicle1.make = "Toyota"   // Sets the make of the 'vehicle1' object to 'Toyota'
+    vehicle1.model = "Corolla" // Assigns the model of the 'vehicle1' object as 'Corolla'
+    vehicle1.color = "blue"    // Changes the color of the 'vehicle1' object to 'blue'
+    vehicle1.engineSize = 1.9f // Updates the engine size of the 'vehicle1' object to '1.9'
+
+    println(vehicle1.make)       // Printing the 'make' attribute of the 'vehicle1' object to the console. Output: Undefined
+    println(vehicle1.model)      // Printing the 'model' attribute of the 'vehicle1' object to the console. Output: Undefined
+    println(vehicle1.color)      // Printing the 'color' attribute of the 'vehicle1' object to the console. Output: Undefined
+    println(vehicle1.engineSize) // Printing the 'engineSize' attribute of the 'vehicle1' object to the console. Output: 0.0
+    vehicle1.driveForwards()     // Invocation of the 'driveForwards' method. Output: Undefined drives forward
+    vehicle1.driveBackwards()    // Invocation of the 'driveBackwards' method. Output: Undefined drives backwards
+    vehicle1.stop()              // Invocation of the 'stop' method. Output: Undefined stops
+}
+
+class Vehicle {
+    // Definition of properties/attributes using variables
+    var make: String = "Undefined"
+    var model: String = "Undefined"
+    var color: String = "Undefined"
+    var engineSize: Float = 0.00f
+
+    // Definition of methods using functions
+    fun driveForwards() {
+        println("$make drives forward")
+    }
+
+    fun driveBackwards() {
+        println("$make drives backwards")
+    }
+
+    fun stop() {
+        println("$make stops")
+    }
+}
