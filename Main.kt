7e0e514bf769a5d6910a8f3badddd7d2fe7d353e
Maria Isabella <isabellaac837@gmail.com>
2026-01-03 13:44:32 -0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // The next lines reserve memory space for 2 different variables.
+    var product = "Tray"
+    println(product)
+
+    product = "Bowl"
+    println(product)
+
+    val product2 = "Disinfectant"
+    println(product2)
+}
