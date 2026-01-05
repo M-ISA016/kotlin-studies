25f5bd9fa658924cb12e379d4021756f92c2af77
Maria Isabella <isabellaac837@gmail.com>
2026-01-04 23:09:26 -0500

    Update and rename Main.kt to b

--- Main.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    println("Insira o nome do usuário.")
-
-    val username: String? = null
-    val nullString = username ?: "Usuário desconhecido"
-
-    println("Bem vindo(a), $nullString!")
-}
