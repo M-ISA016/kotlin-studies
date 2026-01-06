151a0da9ba36dc1783ce080da3f6d9a02bd223fc
Maria Isabella <isabellaac837@gmail.com>
2026-01-05 23:34:57 -0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    helloWorld()
+
+}
+fun helloWorld() {
+    println("Hello world")
+
+fun subtracao() = println(3-2)
+}
+
+// PARAMETERS: functions may or may not have parameters. ex.:
+
+fun ageVerification(age: Int) {
+    if (age > 18)  println("pode dirigir")
+    else println("Não pode dirigir")
+}
